3e145173ecc673eb762536924907c2b81cea6b57
szesex <szekangylp@gmail.com>
2024-08-26 11:37:25 +0000

    Added initial modules and structure

--- src/statics.py
+++ src/statics.py
@@ -1,3 +1,3 @@
 # Statics module
 
-def calculate_centroid(area_coordinates): # Placeholder function to calculate centroid pass def calculate_centroid(area_coordinates): # Placeholder function to calculate centroid pass 
+def calculate_centroid(area_coordinates): # Placeholder function to calculate centroid pass 
